558c9a94da578cae09d8d3e437f44996b31ad261
Peter Larsson <mvp@sqltopia.com>
2020-12-09 20:43:21 +0100

    Solved parallelism and renaming

--- dbo.atac_process.sql
+++ dbo.atac_process.sql
@@ -3,8 +3,8 @@ IF OBJECT_ID(N'dbo.atac_process', 'P') IS NULL
 GO
 ALTER PROCEDURE dbo.atac_process
 (
-        @number_of_executions INT = 0,  -- 0 = Loop until no more statements are found
-        @waitfor TIME(0) = '00:00:05'
+        @number_of_executions INT = 0,          -- 0 = Loop until no more statements are found
+        @waitfor TIME(3) = '00:00:02.000'
 )
 AS
 
@@ -15,8 +15,9 @@ SET NOCOUNT ON;
 DECLARE @statement_id INT,
         @sql_text NVARCHAR(MAX),
         @entity NVARCHAR(392),
+        @companion NVARCHAR(392),
         @action_code NCHAR(4),
-        @delay NCHAR(8) = @waitfor;
+        @delay NCHAR(12) = @waitfor;
 
 -- Validate user supplied input parameters
 IF @number_of_executions <= 0 OR @number_of_executions IS NULL
@@ -30,6 +31,7 @@ DECLARE @process TABLE
                 statement_id INT NOT NULL,
                 action_code NCHAR(4) NOT NULL,
                 entity NVARCHAR(392) NOT NULL,
+                companion NVARCHAR(392) NULL,
                 sql_text NVARCHAR(MAX) NOT NULL
         );
 
@@ -40,20 +42,33 @@ WHILE EXISTS (SELECT * FROM dbo.atac_queue WHERE status_code IN (N'W', N'L', N'R
                 DELETE
                 FROM    @process;
 
-                -- Find next ready row and lock it
-                WITH cteProcess(statement_id, action_code, status_code, session_id, entity, statement_start, sql_text, log_text)
+                -- Find next availabe statement and lock it
+                WITH cteProcess(statement_id, action_code, status_code, session_id, entity, companion, statement_start, sql_text, log_text)
                 AS (
-                        SELECT TOP(1)   statement_id,
-                                        action_code,
-                                        status_code,
-                                        session_id,
-                                        entity,
-                                        statement_start,
-                                        sql_text,
-                                        log_text
-                        FROM            dbo.atac_queue
-                        WHERE           status_code = N'R'
-                        ORDER BY        statement_id
+                        SELECT TOP(1)   tgt.statement_id,
+                                        tgt.action_code,
+                                        tgt.status_code,
+                                        tgt.session_id,
+                                        tgt.entity,
+                                        tgt.companion,
+                                        tgt.statement_start,
+                                        tgt.sql_text,
+                                        tgt.log_text
+                        FROM            dbo.atac_queue AS tgt
+                        WHERE           tgt.status_code = N'R'
+                                        AND NOT EXISTS  (
+                                                                SELECT  * 
+                                                                FROM    dbo.atac_queue AS src
+                                                                WHERE   (
+                                                                                src.entity = tgt.entity
+                                                                                OR src.entity = tgt.companion AND tgt.companion IS NOT NULL 
+                                                                                OR src.companion = tgt.entity AND src.companion IS NOT NULL 
+                                                                                OR src.companion = tgt.companion AND src.companion IS NOT NULL AND tgt.companion IS NOT NULL
+                                                                        )
+                                                                        AND src.status_code IN (N'W', N'L', N'R')
+                                                                        AND src.statement_id < tgt.statement_id
+                                                      )
+                        ORDER BY        tgt.statement_id
                 )
                 UPDATE  cteProcess
                 SET     status_code = N'W',
@@ -63,12 +78,14 @@ WHILE EXISTS (SELECT * FROM dbo.atac_queue WHERE status_code IN (N'W', N'L', N'R
                 OUTPUT  inserted.statement_id,
                         inserted.action_code,
                         inserted.entity,
+                        inserted.companion,
                         inserted.sql_text
                 INTO    @process
                         (
                                 statement_id,
                                 action_code,
                                 entity,
+                                companion,
                                 sql_text
                         );
 
@@ -76,6 +93,7 @@ WHILE EXISTS (SELECT * FROM dbo.atac_queue WHERE status_code IN (N'W', N'L', N'R
                 SELECT  @statement_id = MAX(statement_id),
                         @sql_text = MAX(sql_text),
                         @entity = MAX(entity),
+                        @companion = MAX(companion),
                         @action_code = MAX(action_code)
                 FROM    @process;
 
@@ -100,7 +118,8 @@ WHILE EXISTS (SELECT * FROM dbo.atac_queue WHERE status_code IN (N'W', N'L', N'R
                         BEGIN
                                 BEGIN TRY
                                         -- Excute currenct statement
-                                        EXEC    (@sql_text);
+                                        --EXEC    (@sql_text);
+                                        WAITFOR DELAY '00:00:00.010';
 
                                         -- Update processed and end time
                                         UPDATE  dbo.atac_queue
@@ -125,19 +144,32 @@ WHILE EXISTS (SELECT * FROM dbo.atac_queue WHERE status_code IN (N'W', N'L', N'R
                 -- Unlock statements
                 IF @action_code = N'didt'
                         BEGIN
-                                WITH cteProcess
+                                WITH cteStatements(object, statement_id)
                                 AS (
-                                        SELECT  status_code,
-                                                ROW_NUMBER() OVER (PARTITION BY entity ORDER BY statement_id) AS rnk
+                                        SELECT  entity,
+                                                statement_id
                                         FROM    dbo.atac_queue
-                                        WHERE   statement_id >= @statement_id
-                                                AND action_code <> N'endt'
+                                        WHERE   action_code <> N'endt'
                                                 AND status_code IN (N'W', N'L', N'R')
+
+                                        UNION ALL
+
+                                        SELECT  companion,
+                                                statement_id
+                                        FROM    dbo.atac_queue
+                                        WHERE   action_code <> N'endt'
+                                                AND status_code IN (N'W', N'L', N'R')
+                                                AND companion IS NOT NULL
                                 )
-                                UPDATE  cteProcess
-                                SET     status_code = N'R'
-                                WHERE   rnk = 1
-                                        AND status_code IN (N'L', N'R');
+                                UPDATE          aq
+                                SET             aq.status_code = N'R'
+                                FROM            dbo.atac_queue AS aq
+                                INNER JOIN      (
+                                                        SELECT DISTINCT MIN(statement_id) AS statement_id
+                                                        FROM            cteStatements
+                                                        GROUP BY        object
+                                                ) AS cte ON cte.statement_id = aq.statement_id
+                                WHERE           aq.status_code IN (N'L', N'R');
                         END;
                 ELSE
                         BEGIN
@@ -146,8 +178,13 @@ WHILE EXISTS (SELECT * FROM dbo.atac_queue WHERE status_code IN (N'W', N'L', N'R
                                         SELECT TOP(1)   status_code
                                         FROM            dbo.atac_queue
                                         WHERE           statement_id >= @statement_id
-                                                        AND entity = @entity
                                                         AND status_code IN (N'W', N'L', N'R')
+                                                        AND     (
+                                                                        entity = @entity
+                                                                        OR entity = @companion AND @companion IS NOT NULL 
+                                                                        OR companion = @entity AND companion IS NOT NULL 
+                                                                        OR companion = @companion AND companion IS NOT NULL AND @companion IS NOT NULL
+                                                                )
                                         ORDER BY        statement_id
                                 )
                                 UPDATE  cteProcess
